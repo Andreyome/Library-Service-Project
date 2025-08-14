2ee2288303edf83e1927692f125538962e2e312a
Andrii Omelchenko <andreyomem@gmail.com>
2025-08-14 15:44:46 +0300

    add urls.py for users

--- library_service_project/settings.py
+++ library_service_project/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     "rest_framework",
-    'books',
+    "books",
+    "user"
 ]
 
 MIDDLEWARE = [
@@ -95,6 +96,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+AUTH_USER_MODEL = 'user.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
