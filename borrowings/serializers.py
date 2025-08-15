1e95127bf8f29850d2041c7e742a9b48bae11111
Andrii Omelchenko <andreyomem@gmail.com>
2025-08-15 12:37:07 +0300

    implement create serializer

--- borrowings/serializers.py
+++ borrowings/serializers.py
@@ -1,3 +1,6 @@
+from datetime import date
+
+from django.core.exceptions import ValidationError
 from rest_framework import serializers
 
 from books.serializers import BookDetailSerializer
@@ -16,3 +19,24 @@ class BorrowingDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Borrowing
         fields = ["id", "borrow_date", "expected_return_date", "actual_return_date", "book", "user"]
+
+
+class BorrowingCreateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Borrowing
+        fields = ["expected_return_date","book", "user"]
+
+    def validate(self, data):
+        book = data["book"]
+        if book.inventory == 0:
+            raise ValidationError("Book out of stock")
+        if data["expected_return_date"] < date.today:
+            raise ValidationError("Expected return date cannot be in the past")
+        return data
+
+    def create(self, validated_data):
+        book = validated_data["book"]
+        book.inventory -= 1
+        book.save()
+        return super().create(validated_data)
+
